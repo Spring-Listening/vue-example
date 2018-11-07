5e538f880e6f1d45d89e1b2998bb3a14ef9c7fea
zoucw <zoucw@yunjiglobal.com>
2018-11-07 22:43:47 +0800

    删除依赖包

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,9 @@ module.exports = {
   baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
  
   // outputDir: 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）
-  outputDir: "mycli3",
+  outputDir: "idc",
   //用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
-  assetsDir: "assets",
+  assetsDir: "static",
   //指定生成的 index.html 的输出路径  (打包之后，改变系统默认的index.html的文件名)
   // indexPath: "myIndex.html",
   //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变)
