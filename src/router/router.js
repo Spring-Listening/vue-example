68dabf6a0108a6b34b149146f9a1501d7df08c31
zoucw <zoucw@yunjiglobal.com>
2018-12-10 09:43:28 +0800

    路由

--- src/router/router.js
+++ src/router/router.js
@@ -11,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component:() => import('@/views/Home')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/About.vue')
     }
   ]
 })
